07c2f2b1075ffd2ae02adbcce1ba104a283af5a3
Osvaldo Laini <osvaldolaini@hotmail.com>
2024-03-15 14:12:32 -0300

    master

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
-        <x-favicons-crm></x-favicons-crm>
+        <x-favicons-atrator></x-favicons-atrator>
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
