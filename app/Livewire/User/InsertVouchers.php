a1843fedc747e9cf455b404fb146d09673bc6608
Osvaldo Laini <osvaldolaini@hotmail.com>
2024-04-30 09:30:07 -0300

    master

--- app/Livewire/User/InsertVouchers.php
+++ app/Livewire/User/InsertVouchers.php
@@ -8,10 +8,21 @@ use Livewire\Component;
 
 class InsertVouchers extends Component
 {
+    // Define o layout a ser usado
+    protected $layout = 'lobby';
 
+    public $vouchers;
+    public function mount()
+    {
+        $this->vouchers = Auth::user()->vouchers
+            ->where('active', 1)
+            ->where('limit_access', '>=', date('Y-m-d h:i:s'))
+            ->unique('application');
+    }
     public function render()
     {
-        return view('livewire.user.my-apps')
-        ->layout('layouts.'.$this->layout);
+        return redirect()->to('/lobby')
+            ->with('success', 'Curso adiquirido com sucesso.')
+            ->layout('layouts.' . $this->layout);
     }
 }
