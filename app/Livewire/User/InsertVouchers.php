c45cb2e09a567cc11b313cf3fcc6d876e4fb7979
Osvaldo Laini <osvaldolaini@hotmail.com>
2024-04-29 22:30:33 -0300

    master

--- app/Livewire/User/InsertVouchers.php
+++ app/Livewire/User/InsertVouchers.php
@@ -3,25 +3,17 @@
 namespace App\Livewire\User;
 
 use App\Models\Admin\Course\Course;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Cashier\Http\Controllers\WebhookController;
 use Livewire\Component;
 
 class InsertVouchers extends Component
 {
-    // Define o layout a ser usado
-    protected $layout = 'lobby';
-
-    public $vouchers;
-    public function mount()
+    public function handleWebhook(Request $request)
     {
-        $this->vouchers = Auth::user()->vouchers
-                        ->where('active',1)
-                        ->where('limit_access','>=', date('Y-m-d h:i:s'))
-                        ->unique('application');
-    }
-    public function render()
-    {
-        return view('livewire.user.my-apps')
-        ->layout('layouts.'.$this->layout);
+        // Use Laravel Cashier's WebhookController to handle incoming webhook events
+        return (new WebhookController)->handleWebhook($request);
+        dd($request);
     }
 }
