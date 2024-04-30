bcd944d6daa5df3456f5241ef0c5bf69a04c92d1
Osvaldo Laini <osvaldolaini@hotmail.com>
2024-04-30 16:48:33 -0300

    master

--- app/Livewire/User/InsertVouchers.php
+++ app/Livewire/User/InsertVouchers.php
@@ -2,9 +2,9 @@
 
 namespace App\Livewire\User;
 
-use App\Models\Admin\Course\Course;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Cashier\Cashier;
 use Livewire\Component;
 
 class InsertVouchers extends Component
@@ -15,15 +15,16 @@ class InsertVouchers extends Component
     public $vouchers;
     public function mount(Request $request)
     {
-        dd($request);
 
-        $session_id = $request->get('session_id');
-        $stripe = new \Stripe\StripeClient('sk_test_51Oy0KQP5zhpt1xr74jcSVyLDeVqGvmvMDCQ05vZlF6M8ug3jf1MbTw9CeT2nBnmsGxCQy10zNNLqWZV4KF4Cn1Dy00C4hLyCrE');
-        $stripe->checkout->sessions->retrieve(
-            \Stripe\Stripe::setApiKey(env('STRIPE_SECRET')),
-            []
-        );
 
+        $sessionId = $request->get('session_id');
+
+        if ($sessionId === null) {
+            return;
+        }
+
+        $session = Cashier::stripe()->checkout->sessions->retrieve($sessionId);
+        dd($session);
         $this->vouchers = Auth::user()->vouchers
             ->where('active', 1)
             ->where('limit_access', '>=', date('Y-m-d h:i:s'))
