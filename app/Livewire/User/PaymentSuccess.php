cd9372f8fefd12af6b11f3333e1b42efc837c044
Osvaldo Laini <osvaldolaini@hotmail.com>
2024-04-30 18:28:56 -0300

    master

--- app/Livewire/User/PaymentSuccess.php
+++ app/Livewire/User/PaymentSuccess.php
@@ -5,8 +5,11 @@ namespace App\Livewire\User;
 use App\Models\Admin\Course\Course;
 use App\Models\Admin\Course\PackPivotCourse;
 use App\Models\Admin\Voucher\Vouchers;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Laravel\Cashier\Cashier;
 use Livewire\Component;
 use Illuminate\Support\Str;
@@ -26,12 +29,22 @@ class PaymentSuccess extends Component
         }
 
         $session = Cashier::stripe()->checkout->sessions->retrieve($sessionId);
-        dd($session);
         if ($session->payment_status !== 'paid') {
             return;
         }
 
         $pack_id = $session['metadata']['pack_id'] ?? null;
+        $custumer = $session['customer_details'];
+        if (User::where('email',$custumer['email'])->get()) {
+            $user = User::where('email',$custumer['email'])->get();
+        }else{
+            $user =  User::create([
+                'name' => $session['customer_details']['name'],
+                'email' => $session['customer_details']['email'],
+                'password' => Hash::make(123456789),
+                'group'=>'user'
+            ]);
+        }
 
         $pack = PackPivotCourse::findOrFail($pack_id);
 
@@ -39,7 +52,7 @@ class PaymentSuccess extends Component
             foreach ($pack->package as $voucher) {
                 Vouchers::create([
                     'plan_id'       =>$voucher->plan_id,
-                    'user_id'       =>$this->user_id,
+                    'user_id'       =>$user->id,
                     'course_id'     =>$voucher->course_id,
                     'application'   =>$voucher->application,
                     'active'        => 1,
